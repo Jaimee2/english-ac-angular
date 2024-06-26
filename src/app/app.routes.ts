678a25a4f36141a89e25c2278b79d1f9aef7961f
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-06-26 13:36:56 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import {Routes} from '@angular/router';
 import {RegistrationFromComponent} from "./registration-from/registration-from.component";
 
 export const routes: Routes = [
-  {path: 'registration', component: RegistrationFromComponent}
+  {path: 'registration', component: RegistrationFromComponent},
+  {path: '**', component: RegistrationFromComponent},
 ];
