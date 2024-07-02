d8b9f90397276cfabba40962bfd23bcf671f9c3f
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-07-02 16:15:27 +0200

    added go back in student detail

--- src/app/student/student-detail/student-detail.component.ts
+++ src/app/student/student-detail/student-detail.component.ts
@@ -4,23 +4,25 @@ import {ActivatedRoute} from "@angular/router";
 import {MatInput} from "@angular/material/input";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {StudentsService} from "../students.service";
-import {dateComparator} from "@ng-bootstrap/ng-bootstrap/datepicker/datepicker-tools";
+import {Location} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-student-detail',
   standalone: true,
-  imports: [
-    MatFormField,
-    MatInput,
-    MatLabel,
-    MatCard,
-    MatCardHeader,
-    MatCardContent,
-    MatCardTitle,
-  ],
+  imports: [MatFormField, MatInput, MatLabel, MatCard, MatCardHeader, MatCardContent, MatCardTitle, MatIcon, MatIconButton,],
   template: `
 
     <div class="container mt-4">
+
+      <div class="row mb-3">
+        <div class="col-12">
+          <button (click)="goBack()" mat-icon-button>
+            <mat-icon>arrow_back</mat-icon>
+          </button>
+        </div>
+      </div>
 
       <div class="row">
         <div class="col-md-4 text-center">
@@ -117,33 +119,26 @@ import {dateComparator} from "@ng-bootstrap/ng-bootstrap/datepicker/datepicker-t
   `]
 })
 export class StudentDetailComponent implements OnInit {
+
   route = inject(ActivatedRoute);
   studentService = inject(StudentsService);
+  _location = inject(Location);
 
   studentId!: string;
   student!: any;
 
   ngOnInit(): void {
     this.studentId = this.route.snapshot.paramMap.get('id')!;
+
     this.studentService.getStudentDetail(this.studentId).subscribe(data => {
       this.student = data;
     });
-    // this.student = {
-    //   id: '1',
-    //   firstName: 'Jaime',
-    //   lastName: 'Higueras',
-    //   parentPhone: '5989899',
-    //   parentEmail: 'jaime@Gmail.com',
-    //   siblings: 'No',
-    //   payment: 'cash',
-    //   studentStatus: 'new',
-    //   imageUrl: '',
     //   studentClass: 'A2', // Example class
-    //   schedule: 'Mon-Fri 8:00am - 3:00pm' // Example schedule
-    // };
+    //   schedule: 'Mon-Fri 8:00am - 3:00pm'
   }
 
 
-
-
+  goBack() {
+    this._location.back();
+  }
 }
